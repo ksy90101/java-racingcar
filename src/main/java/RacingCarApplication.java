48a626f5c868bf16d4955c484e2b28c9329eb5d0
SeYun <ksy90101@naver.com>
2020-06-22 13:54:08 +0900

    feat : InputView 생성

---
+++ src/main/java/RacingCarApplication.java
@@ -0,0 +1,8 @@
+import view.InputView;
+
+public class RacingCarApplication {
+	public static void main(String[] args) {
+		InputView.inputCarNames();
+		InputView.inputGameCount();
+	}
+}
---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,21 @@
+package view;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class InputView {
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static List<String> inputCarNames(){
+		System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+		String inputNames = scanner.nextLine();
+		return Arrays.stream(inputNames.split(",")).collect(Collectors.toList());
+	}
+
+	public static String inputGameCount(){
+		System.out.println("시도할 회수는 몇회인가요?");
+		return scanner.nextLine();
+	}
+}
